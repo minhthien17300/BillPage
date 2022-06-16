4590a2e23aa7c3c995609eabe8e1d174697b23e2
minhthien17300 <minhthien17300@gmail.com>
2022-06-16 18:21:01 +0700

    dsds

--- src/Bill.js
+++ src/Bill.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import "./Bill.css";
-import {post} from './fetch'
+import fetch from './fetch'
 import {
     Button,
     Card,
@@ -33,7 +33,7 @@ export default function Bill() {
         (
             async () => {
                 const id = GetURLParameter('id');
-                const response = await post('http://localhost:5000/order/paymentConfirm', { id: id });
+                const response = await fetch.post('http://localhost:5000/order/paymentConfirm', { id: id });
                 if (response.success) {
                     console.log("success")
                 }
